46f4b31ecc264d81ae614b8a61a01dbc5b1be9d6
dmitriy-pc <dimontura@yandex.ru>
2023-03-29 09:49:02 +0700

    feat: в список Транзакций добавил фильтр по user_id и сортировку по сумме средств и дате создания

--- routers/transactions.py
+++ routers/transactions.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from db import models
 from db.schemas import TransactionSchema
 from dependencies import get_db, get_transaction
+from sqlalchemy import desc
 
 router = APIRouter(
     prefix="/api/transactions",
@@ -10,10 +11,21 @@ router = APIRouter(
 
 
 @router.get("/", response_model=list[TransactionSchema])
-def transactions_list(db: Session = Depends(get_db)) -> list[models.Transaction]:
+def transactions_list(
+    user_id: int | None = None,
+    sort_by_amount: bool = False,
+    sort_by_date: bool = False,
+    db: Session = Depends(get_db),
+) -> list[models.Transaction]:
     """Получить список Транзакций."""
-    transactions_db = db.query(models.Transaction).all()
-    return transactions_db
+    transactions_db = db.query(models.Transaction)
+    if user_id:
+        transactions_db = transactions_db.filter_by(user_id=user_id)
+    if sort_by_amount:
+        transactions_db = transactions_db.order_by(desc("amount"))
+    if sort_by_date:
+        transactions_db = transactions_db.order_by(desc("created_at"))
+    return transactions_db.all()
 
 
 @router.get("/{transaction_id}/", response_model=TransactionSchema)
--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -1,6 +1,6 @@
 from fastapi import status
 from db.models import User, Transaction
-from .factories import transaction_factory
+from .factories import transaction_factory, user_factory
 
 
 def test_transaction_list(test_db, client, db_session):
@@ -43,3 +43,33 @@ def test_transaction_detail_error(test_db, client, db_session):
     response = client.get(url)
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json() == {"detail": f"Транзакция с ID {wrong_id} не найдена."}
+
+
+def test_transaction_list_filters(test_db, client, db_session):
+    user1 = user_factory(db_session).create()
+    user2 = user_factory(db_session).create()
+    amount1 = 10
+    amount2 = 20
+    transaction1 = transaction_factory(db_session).create(user=user1, amount=amount1)
+    transaction2 = transaction_factory(db_session).create(user=user1, amount=amount2)
+    transaction3 = transaction_factory(db_session).create(user=user1, amount=amount2)
+    transaction4 = transaction_factory(db_session).create(user=user2, amount=amount2)
+    db_session.commit()
+
+    url = f"/api/transactions/"
+
+    response = client.get(url + f"?user_id={user1.id}")
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.json()) == 3
+
+    response = client.get(url + f"?user_id={user2.id}")
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.json()) == 1
+
+    response = client.get(url + "?sort_by_amount=True")
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.json()) == 4
+
+    response = client.get(url + "?sort_by_date=True")
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.json()) == 4
