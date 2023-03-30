2840da247eafe441baadc1305955fe60f8bbd3ec
dmitriy-laptop <dimontura@yandex.ru>
2023-03-30 23:13:20 +0700

    feat: добавил эндпоинт со статистикой по Услугам на основании Транзакций

--- routers/transactions.py
+++ routers/transactions.py
@@ -4,6 +4,9 @@ from db import models
 from db.schemas import TransactionSchema
 from dependencies import get_db, get_transaction
 from sqlalchemy import desc
+import sqlalchemy
+from sqlalchemy import func
+
 
 router = APIRouter(
     prefix="/api/transactions",
@@ -13,6 +16,7 @@ router = APIRouter(
 @router.get("/", response_model=list[TransactionSchema])
 def transactions_list(
     user_id: int | None = None,
+    service_id: str | None = None,
     sort_by_amount: bool = False,
     sort_by_date: bool = False,
     db: Session = Depends(get_db),
@@ -21,11 +25,35 @@ def transactions_list(
     transactions_db = db.query(models.Transaction)
     if user_id:
         transactions_db = transactions_db.filter_by(user_id=user_id)
+    if service_id:
+        transactions_db = transactions_db.filter_by(service_id=service_id)
     if sort_by_amount:
         transactions_db = transactions_db.order_by(desc("amount"))
     if sort_by_date:
         transactions_db = transactions_db.order_by(desc("created_at"))
     return transactions_db.all()
+
+
+@router.get("/services_statistics/")
+def transactions_services_statistics(
+    month: int | None = None, year: int | None = None, db: Session = Depends(get_db)
+) -> dict:
+    """Статистика по Услугам на основании Транзакций."""
+    qs = db.query(
+        models.Transaction.service_id.label("service_id"),
+        func.count(models.Transaction.service_id).label("count"),
+        func.sum(models.Transaction.amount).label("sum"),
+    ).group_by(models.Transaction.service_id)
+    if month:
+        qs = qs.filter(
+            sqlalchemy.extract("month", models.Transaction.created_at) == month
+        )
+    if year:
+        qs = qs.filter(
+            sqlalchemy.extract("year", models.Transaction.created_at) == year
+        )
+    qs = qs.all()
+    return qs
 
 
 @router.get("/{transaction_id}/", response_model=TransactionSchema)
